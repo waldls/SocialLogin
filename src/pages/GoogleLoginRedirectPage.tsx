50ee7cf4d03b59d27b8ea1f6ae2bc03ef08558df
waldls <p353700@gmail.com>
2025-07-21 01:53:26 +0900

    [CHORE] 주석 추가

--- src/pages/GoogleLoginRedirectPage.tsx
+++ src/pages/GoogleLoginRedirectPage.tsx
@@ -5,29 +5,33 @@ import { LOCAL_STORAGE_KEY } from "../constants/key";
 
 const GoogleLoginRedirectPage = () => {
   const navigate = useNavigate();
+
+  // 커스텀 훅을 통해 accessToken 저장 함수 호출
   const { setItem: setAccessToken } = useLocalStorage(
     LOCAL_STORAGE_KEY.accessToken
   );
+  // refreshToken 저장 함수 호출
   const { setItem: setRefreshToken } = useLocalStorage(
     LOCAL_STORAGE_KEY.refreshToken
   );
 
   useEffect(() => {
+    // 현재 URL의 쿼리 파라미터 추출
     const queryParams = new URLSearchParams(window.location.search);
     const accessToken = queryParams.get("accessToken");
     const refreshToken = queryParams.get("refreshToken");
 
     if (accessToken && refreshToken) {
-      // 토큰 둘 다 저장
+      // accessToken과 refreshToken이 모두 존재할 경우 저장
       setAccessToken(accessToken);
       setRefreshToken(refreshToken);
 
-      // 저장 후 /my 페이지 이동
+      // 약간의 딜레이 후 /my 페이지로 이동 (스토리지 저장 안정성 확보 목적)
       setTimeout(() => {
         navigate("/my");
       }, 100);
     } else {
-      // 하나라도 없으면 로그인 페이지로
+      // 둘 중 하나라도 없으면 로그인 페이지로 이동
       navigate("/login");
     }
   }, [navigate, setAccessToken, setRefreshToken]);
